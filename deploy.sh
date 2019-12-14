cf5f407a7b3ba90fbe5b3f943b00fb847bd35572
Jialin Ma <marlin@inventati.org>
2019-12-14 05:06:19 -0500

    Add deploy.sh and index.html

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -x
+
+if [ "$1" = "" ]; then
+    echo "Usage: ./deploy.sh commit"
+    exit 1
+fi
+
+REPO="symengine/symengine.R"
+COMMIT="$1"
+
+wget https://github.com/"$REPO"/archive/"$COMMIT".tar.gz
+tar xvf "$COMMIT".tar.gz
+mv symengine.R-"$COMMIT" src-"$COMMIT"
+
+cd src-"$COMMIT"
+./tools/build_doc_site.R ../$COMMIT
